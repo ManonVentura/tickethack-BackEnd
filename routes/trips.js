3e596db3122660217ff8b37744ffd7c71a5ecf1c
Manon <m.ventura@vatel.fr>
2022-09-21 11:04:59 +0200

    get trips from request done

--- routes/trips.js
+++ routes/trips.js
@@ -1,28 +1,27 @@
 var express = require('express');
 var router = express.Router();
+const moment = require('moment');
 require('../models/connection');
 const Trip = require('../models/trips');
 
 /* GET all the trips with 3 parameters : departure, arrival and date. */
 //! ajouter la date!!!!!!!
-router.get('/', function(req, res, next) {
-    // Trip.find()
-  //Trip.find({ departure: req.query.departure, arrival:req.query.arrival})
-  Trip.find({departure: { $regex: new RegExp(req.query.departure, 'i')}, arrival: { $regex: new RegExp(req.query.arrival, 'i')} /* date: req.query.date */ })
+router.get('/:departure/:arrival/:date', function(req, res, next) {
+  const { departure, arrival, date } = req.params
+  Trip.find({
+    departure: { $regex: new RegExp(departure, 'i')}, 
+    arrival: { $regex: new RegExp(arrival, 'i')},  
+    date: { $gte : moment(date).startOf('day'), $lte : moment(date).endOf('day')} 
+})
   .then(dbData => {
-    console.log('db data: ', dbData)
-    if (dbData === [null]) {
-
-      console.log('if')
-      res.json({ result: false, error: 'Sorry there is no trip available between those cities at those dates' })
+    if (dbData.length === 0) {
+      res.json({ result: false, error: `Sorry there is no trip available between ${departure} and ${arrival} at those dates` })
       
       
     } else {
-      console.log('else')
       
       res.json({trips : dbData})
     
-        // res.json({error : 'Sorry there is no trip available between those cities at those dates'})
       }
    })
 });
